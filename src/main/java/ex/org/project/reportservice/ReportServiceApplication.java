e12861779ce35319713f3d983fd6c9601b3ef010
Attila L. Egyedi <attila.egyedi@gmail.com>
2025-09-29 18:49:40 -0700

    Add remote stop capability for DEV

--- src/main/java/ex/org/project/reportservice/ReportServiceApplication.java
+++ src/main/java/ex/org/project/reportservice/ReportServiceApplication.java
@@ -7,16 +7,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"edu.stanford.bmir.radx.harmonization.metrics", "gov.nih.radx.reportservice"})
+@ComponentScan(basePackages = {
+    "edu.stanford.bmir.radx.harmonization.metrics",
+    "gov.nih.radx.reportservice",
+    "ex.org.project.reportservice.security"
+})
 public class ReportServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReportServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ReportServiceApplication.class, args);
+  }
 
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
+  @Bean
+  public RestTemplate getRestTemplate() {
+    return new RestTemplate();
+  }
 
 }
