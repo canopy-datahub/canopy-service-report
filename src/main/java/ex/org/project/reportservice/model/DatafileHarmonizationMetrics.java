d6f9c5d3d6482ebbf80ae6a2aeec0660554598e8
Yan Cao <ycao77@stanford.edu>
2025-10-09 13:02:30 -0700

    remove commented tier 2 and 3 variables

--- src/main/java/ex/org/project/reportservice/model/DatafileHarmonizationMetrics.java
+++ src/main/java/ex/org/project/reportservice/model/DatafileHarmonizationMetrics.java
@@ -41,26 +41,8 @@ public class DatafileHarmonizationMetrics {
     @Column(name = "harmonizable_tier_1_variable_count")
     private Integer harmonizableTier1VariableCount;
 
-//    @Column(name = "harmonizable_tier_2_variable_count")
-//    private Integer harmonizableTier2VariableCount;
-//
-//    @Column(name = "harmonizable_tier_3_variable_count")
-//    private Integer harmonizableTier3VariableCount;
-//
-//    @Column(name = "harmonizable_total")
-//    private Integer harmonizableTotal;
-
     @Column(name = "harmonized_tier_1_variable_count")
     private Integer harmonizedTier1VariableCount;
-
-//    @Column(name = "harmonized_tier_2_variable_count")
-//    private Integer harmonizedTier2VariableCount;
-//
-//    @Column(name = "harmonized_tier_3_variable_count")
-//    private Integer harmonizedTier3VariableCount;
-//
-//    @Column(name = "harmonized_total")
-//    private Integer harmonizedTotal;
 
     @Column(name = "orig_variables")
     private String origVariables;
@@ -71,20 +53,8 @@ public class DatafileHarmonizationMetrics {
     @Column(name = "harmonizable_tier_1_variables")
     private String harmonizableTier1Variables;
 
-//    @Column(name = "harmonizable_tier_2_variables")
-//    private String harmonizableTier2Variables;
-//
-//    @Column(name = "harmonizable_tier_3_variables")
-//    private String harmonizableTier3Variables;
-
     @Column(name = "harmonized_tier_1_variables")
     private String harmonizedTier1Variables;
-
-//    @Column(name = "harmonized_tier_2_variables")
-//    private String harmonizedTier2Variables;
-//
-//    @Column(name = "harmonized_tier_3_variables")
-//    private String harmonizedTier3Variables;
 
     public DatafileHarmonizationMetrics(Integer reportId, OrigTransformFilePairMetrics pairMetrics){
         this.reportId = reportId;
@@ -95,28 +65,14 @@ public class DatafileHarmonizationMetrics {
         this.origVariableCount = pairMetrics.nDataElementsOrig();
         this.transformVariableCount = pairMetrics.nDataElementsTransform();
         this.harmonizableTier1VariableCount = pairMetrics.nHarmonizableDataElementsTier1();
-//        this.harmonizableTier2VariableCount = pairMetrics.nHarmonizableDataElementsTier2();
-//        this.harmonizableTier3VariableCount = pairMetrics.nHarmonizableDataElementsTier3();
-//        this.harmonizableTotal = pairMetrics.totalHarmonizable();
         this.harmonizedTier1VariableCount = pairMetrics.nHarmonizedDataElementsTier1();
-//        this.harmonizedTier2VariableCount = pairMetrics.nHarmonizedDataElementsTier2();
-//        this.harmonizedTier3VariableCount = pairMetrics.nHarmonizedDataElementsTier3();
-//        this.harmonizedTotal = pairMetrics.totalHarmonized();
         this.origVariables = HarmonizationCalculatorUtil.concatenateSet(
                 pairMetrics.dataElementsOrig(), ",");
         this.transformVariables = HarmonizationCalculatorUtil.concatenateSet(
                 pairMetrics.dataElementsTransform(), ",");
         this.harmonizableTier1Variables = HarmonizationCalculatorUtil.concatenateSet(
                 pairMetrics.harmonizableDataElementsTier1(), ",");
-//        this.harmonizableTier2Variables = HarmonizationCalculatorUtil.concatenateSet(
-//                pairMetrics.harmonizableDataElementsTier2(), ",");
-//        this.harmonizableTier3Variables = HarmonizationCalculatorUtil.concatenateSet(
-//                pairMetrics.harmonizableDataElementsTier3(), ",");
         this.harmonizedTier1Variables = HarmonizationCalculatorUtil.concatenateSet(
                 pairMetrics.harmonizedDataElementsTier1(), ",");
-//        this.harmonizedTier2Variables = HarmonizationCalculatorUtil.concatenateSet(
-//                pairMetrics.harmonizedDataElementsTier2(), ",");
-//        this.harmonizedTier3Variables = HarmonizationCalculatorUtil.concatenateSet(
-//                pairMetrics.harmonizedDataElementsTier3(), ",");
     }
 }
